b4037ccb470196e6b977406a27c681fbd6d78666
HelloAvenir <102899881+helloavenir@users.noreply.github.com>
2022-05-02 17:38:40 +0900

    Add files via upload

---
+++ p151_12_yearZodiac_0502.py
@@ -0,0 +1,25 @@
+year = int(input('연도를 입력하시오 : '))
+if year%12 == 0 :
+    print('원숭이띠입니다.')
+elif year%12 == 1 :
+    print('닭띠입니다.')
+elif year%12 == 2 :
+    print('개띠입니다.')
+elif year%12 == 3 :
+    print('돼지띠입니다.')
+elif year%12 == 4 :
+    print('쥐띠입니다.')
+elif year%12 == 5 :
+    print('소띠입니다.')
+elif year%12 == 6 :
+    print('범띠입니다.')
+elif year%12 == 7 :
+    print('토끼띠입니다.')
+elif year%12 == 8 :
+    print('용띠입니다.')
+elif year%12 == 9 :
+    print('뱀띠입니다.')
+elif year%12 == 10 :
+    print('말띠입니다.')
+else :
+    print('양띠입니다.')
