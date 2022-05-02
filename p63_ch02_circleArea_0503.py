dde2da38c6eae590690c7c743533b081d773dc8e
HelloAvenir <102899881+helloavenir@users.noreply.github.com>
2022-05-03 07:48:25 +0900

    Add files via upload

--- p63_ch02_circleArea_0503.py
+++ p63_ch02_circleArea_0503.py
@@ -1,3 +1,14 @@
 radius = float(input('원의 반지름은? '))
 area = 3.14 * radius ** 2
 print('반지름 %.2f인 원의 면적 = %.2f' %(radius, area))
+print()
+
+
+radiusRound = '{:0,.2f}'.format(radius)
+areaRound = '{:0,.2f}'.format(area)
+
+print('반지름', radius, '인 원의 면적= ', area)
+print()
+
+
+print('반지름', radiusRound, '인 원의 면적= ', areaRound)
